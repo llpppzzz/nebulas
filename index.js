4d94f6eec67570aa95bf9bb304c08255253b0a71
lipeizhou <llpppzzz@163.com>
2018-11-29 15:46:11 +0800

    接口支持分页和排序

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 class MyContract {
   constructor () {
-
   }
 
   init () {
-
   }
 
-  getAllItems () {
-    return LocalContractStorage.get('item')
+  _sort (arr, field, dir) {
+    if (!dir || !field) return arr
+    return arr.sort((a, b) => dir === 'descending' ? b[field] - a[field] : a[field] - b[field])
+  }
+
+  getAllItems ({sort = 'published_at', dir, page, pageSize}) {
+    const data = LocalContractStorage.get('item') || []
+    const sorted = this._sort(data, sort, dir)
+    const startPage = --page * pageSize
+    const list = sorted.slice(startPage, startPage + pageSize)
+    return {
+      list,
+      page: ++page,
+      pageSize,
+      total: data.length
+    }
   }
 
   setItem (content) {
@@ -16,7 +28,7 @@ class MyContract {
       content,
       published_at: +new Date()
     }
-    const items = LocalContractStorage.get('item')
+    const items = LocalContractStorage.get('item') || []
     items.push(newItem)
     LocalContractStorage.set('item', items)
     return newItem
